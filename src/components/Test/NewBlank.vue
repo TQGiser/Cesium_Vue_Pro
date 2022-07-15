6043833ff570af0031370d620aeeca62a7c10da0
TQGiser <540484369@qq.com>
2022-07-15 17:22:07 +0800

    a

--- src/components/Test/NewBlank.vue
+++ src/components/Test/NewBlank.vue
@@ -20,7 +20,7 @@ export default {
             jd: null,
             wd: null,
             sjg: null,
-            ddg:null
+            ddg: null
         });
         Cesium.Ion.defaultAccessToken =
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0MjczNDgzMy1hYzE1LTRjNWYtODZhMS01MjZkNWRiMDc2MmUiLCJpZCI6ODIxMzAsImlhdCI6MTY0NDU0ODc0M30.LpGXXWsbQXucV5MTeC2g8BCAQWiZp612gosWcK-7ocE";
@@ -33,6 +33,11 @@ export default {
                     requestWaterMask: true,
                     requestVertexNormals: true,
                 }),
+                // terrainProvider: new Cesium.CesiumTerrainProvider({
+                //     url: "http://localhost:8083/terrain/甘孜地形切片/巴塘县",
+                //     minimumLevel: 0,
+                //     maximumLevel: 15,
+                // }),
             });
             state.viewer.camera.flyTo({
                 destination: Cesium.Cartesian3.fromDegrees(
